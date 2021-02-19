8379d74392b19db068a93f578f4db2320a20f0db
admin <shaahid@macappstudio.com>
2021-02-19 10:36:44 +0530

    signup empty field reg prob cleared

--- projectTwo/SignupView.swift
+++ projectTwo/SignupView.swift
@@ -15,7 +15,9 @@ class SignupView: UIViewController,UITextFieldDelegate {
     @IBOutlet weak var passTextF: UITextField!
     @IBOutlet weak var SignupBtn: UIButton!
     @IBOutlet weak var messageLabel: UILabel!
+    
     @IBAction func signupTapped(_ sender: UIButton) {
+        if nameTextF.text?.isEmpty != true && userNameTextF.text?.isEmpty != true && passTextF.text?.isEmpty != true {
     let request = NSMutableURLRequest(url: NSURL(string: "https://appstudio.co/iOS/orgReg.php")! as URL)
     request.httpMethod = "POST"
     let postString = "name=\(nameTextF.text!)&username=\(userNameTextF.text!)&password=\(passTextF.text!)"
@@ -31,14 +33,35 @@ class SignupView: UIViewController,UITextFieldDelegate {
         print("responseString = \(String(describing: responseString))")
         }
         task.resume()
-        let alertController = UIAlertController(title: "User's Detail", message: "Successfully Added", preferredStyle: UIAlertController.Style.alert)
+       /* let alertController = UIAlertController(title: "User's Detail", message: "Successfully Added", preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default,handler: nil))
-        self.present(alertController, animated: true, completion: nil)
+        self.present(alertController, animated: true, completion: nil)*/
+        
+            
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let navigationController = storyBoard.instantiateViewController(withIdentifier: "HomeNavigationContoller") as! UINavigationController
+        let vc = storyBoard.instantiateViewController(withIdentifier: "HomeView") as! TableViewController
+        navigationController.pushViewController(vc, animated: true)
+        navigationController.modalPresentationStyle = .fullScreen
+        self.present(navigationController, animated: true, completion: nil)
+            vc.getusername = userNameTextF.text
         nameTextF.text = ""
         userNameTextF.text = ""
         passTextF.text = ""
         nameTextF.becomeFirstResponder()
-    }
+        
+        }else {
+            let alert = UIAlertController(title: "Alert", message: "Fill All Fields", preferredStyle: UIAlertController.Style.alert)
+            let cancel = UIAlertAction(title: "Ok", style: .cancel) { (action) -> Void in
+              }
+            alert.addAction(cancel)
+            present(alert, animated: true, completion: nil)
+            nameTextF.text = ""
+            userNameTextF.text = ""
+            passTextF.text = ""
+            nameTextF.becomeFirstResponder()
+          }
+        }
     
     override func viewDidLoad() {
         super.viewDidLoad()
